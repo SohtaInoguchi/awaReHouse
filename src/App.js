b6231a7b486319b069e1d5dfe5b753d12a2ae025
Tonipenyallop <kimtesu521@gmail.com>
2022-03-01 19:30:05 +0900

    add: tailwind config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./input.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="text-green-500 flex justify-center items-center flex-col">
+        {" "}
+        super
+        <div className="class-name-is-here group">
+          hehehe
+          <div className="toni group-hover:scale-100">NOICE</div>
+        </div>
+      </div>
     </div>
   );
 }
