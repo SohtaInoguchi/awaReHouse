b63b1fd83245b214cbe625159fb825b9158a018f
Tonipenyallop <kimtesu521@gmail.com>
2022-03-03 12:15:21 +0900

    organize

--- src/components/Userpage.js
+++ src/components/Userpage.js
@@ -1,8 +1,15 @@
 import "../input.css";
+import Success from "../components/Success";
+import Subscription from "../components/Subscription";
 import { useState, useEffect } from "react";
 
-function Userpage() {
-  return <div></div>;
+function Userpage({ user, message, success }) {
+  return (
+    <div>
+      Welcome {user}
+      {success === true ? <Success message={message} /> : <Subscription />}
+    </div>
+  );
 }
 
 export default Userpage;
