6c516ba8d8f4b6f57c336e1a9621ee6d7def0e68
JohnnyDamon <53366209+JohnnyDamon@users.noreply.github.com>
2022-03-03 11:21:39 +0900

    debug: changed env

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ const PORT = process.env.PORT || 8000;
 // socket io
 const http = require('http');
 const server = http.createServer(app);
-const socketioPORT = server.address.port || 7777;
-// const socketioPORT = 7777;
+
+// const socketioPORT = server.address.port || 7777;
+const socketioPORT = 7777;
+// const socketioPORT = PORT;
 const originURL = process.env.URL || ['http://localhost:8000'];
+
+// const io = require('socket.io')(7777);
+// console.log(io);
 const io = require('socket.io')(socketioPORT, {
 // const io = require('socket.io')(server.address.port, {
   cors: {
@@ -17,13 +22,17 @@ const io = require('socket.io')(socketioPORT, {
     methods: ["GET", "POST"]
   }
 });
+// io.configure(function () { 
+//   io.set("transports", ["xhr-polling"]); 
+//   io.set("polling duration", 10); 
+// });
+
 
 app.use(express.json());
 app.use(cors());
 app.use(express.static(__dirname + "/build"));
 
 app.get("/", (_, res) => {
-  
   res.send("hehehehe");
 });
 
