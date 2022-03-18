660b7dd39a2153e815e2f5e3311d21783149a57d
DevinAnnunzio <annunzio.devin@gmail.com>
2022-03-18 10:08:18 +0900

    Chore: merged commits

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,11 +3,6 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { BoxFlow } from "./BoxFlow";
 import Response from "./Response";
-<<<<<<< HEAD
-import { FaUserTie, FaUser, FaUserShield } from "react-icons/fa";
-import { ExplanationPage } from "./ExplanationPage";
-=======
->>>>>>> 37d7d50bd635911de2760c8655791a54e615fa18
 
 import LearnMore from "./LearnMore";
 import MoreLife from "./MoreLife";
@@ -60,93 +55,7 @@ export default function Homepage({
       ) : (
         <div className="flex flex-wrap justify-center items-center ">
           {" "}
-<<<<<<< HEAD
-          {/* <div className="homepageContainer flex justify-center items-center flex-wrap ">
-            <img
-              className="top-0 w-36 h-36 rounded-3xl"
-              //  w-20 h-20
-              src={require("../pictures/LOGO.png")}
-              alt=""
-            />
-
-            <div className="flex flex-row flex-wrap ">
-              <div className="flex ">
-                <div className="flex justify-center items-center">
-                  <button
-                    className="login-button"
-                    onClick={() => {
-                      console.log(
-                        window.localStorage.getItem("firstName_user")
-                      );
-                      if (window.localStorage.getItem("firstName_user")) {
-                        window.localStorage.removeItem("firstName_provider");
-                        window.localStorage.removeItem("email_provider");
-                        window.localStorage.removeItem("token_provider");
-                        navigate("/user");
-                      } else navigate("/login/user");
-                    }}
-                  >
-                    <Icon icon={<FaUser size="24" />} />
-                    User
-                  </button>
-                </div>
-
-                <div className="">
-                  <button
-                    className="login-button"
-                    onClick={() => {
-                      console.log(
-                        window.localStorage.getItem("firstName_provider")
-                      );
-                      if (window.localStorage.getItem("firstName_provider")) {
-                        window.localStorage.removeItem("firstName_user");
-                        window.localStorage.removeItem("email_user");
-                        window.localStorage.removeItem("token_user");
-                        navigate("/provider");
-                      } else navigate("/login/provider");
-                    }}
-                  >
-                    <Icon icon={<FaUserTie size="24" />} />
-                    Provider
-                  </button>
-                </div>
-
-                <button
-                  className="login-button"
-                  onClick={() => navigate("admin")}
-                >
-                  <Icon icon={<FaUserShield size="24" />} />
-                  Admin
-                </button>
-              </div>
-            </div>
-          </div> */}
-          <ExplanationPage/>
-          <div className="flex flex-row justify-center  text-white rounded-xl text-center w-full  flex-wrap  ">
-            <img
-              className=" object-contain w-auto rounded-tl-3xl rounded-bl-3xl opacity-80"
-              // src={require("../pictures/homepage-img.jpeg")}
-              src={require("../pictures/clean-house.jpeg")}
-              alt=""
-            />
-            <div className="flex flex-col justify-center items-center w-60 bg-gray-100 text-blue-500 text-center  rounded-tr-3xl rounded-br-3xl border-2 ">
-              <h2>What is awaReHouse?</h2>
-              <p className="break-words">
-                awaReHouse is a service connecting private storage providers to
-                storage seekers in Japan and is specifically designed for
-                seasonal items
-              </p>
-              <p
-                className="rounded-3xl text-blue-600 bg-slate-300 px-2 py-2 mx-2 my-2 hover:bg-slate-400 hover:text-blue-700 cursor-pointer"
-                onClick={() => navigate("/learn")}
-              >
-                Learn more
-              </p>
-            </div>
-          </div>
-=======
           <MoreLife />
->>>>>>> 37d7d50bd635911de2760c8655791a54e615fa18
           <div
             id="slogan"
             className="flex flex-col
