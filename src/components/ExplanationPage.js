f8ba62f8cf430e7af282b9f3aae8bb000f679794
Tonipenyallop <kimtesu521@gmail.com>
2022-03-21 18:07:37 +0900

    feat:add start section

--- src/components/ExplanationPage.js
+++ src/components/ExplanationPage.js
@@ -1,37 +1,9 @@
 import * as React from "react";
-import { useState } from "react";
-// import Button from "@mui/material/Button";
-import { Button, Form } from "react-bootstrap";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
 import PlanSelection from "./PlanSelection";
 export default function ExplanationPage({ plan, setPlan }) {
-  const navigate = useNavigate();
-
-  const userEmail = window.localStorage.getItem("email_user");
-
   return (
     <div>
-      <h3 className="bg-white px-3 py-3 text-blue-600 text-center">
-        Please make a selection below for your plan
-      </h3>
       <PlanSelection plan={plan} setPlan={setPlan} />
-      <div className="buttons  flex justify-center items-center my-3 ">
-        <Form action="/create-checkout-session" method="POST">
-          <Button className="mx-10" onClick={() => navigate("/")}>
-            Cancel
-          </Button>
-          <Button
-            className="mx-10"
-            name="name"
-            value="Storage fee"
-            type="submit"
-            onClick={() => axios.post(`/login/verify/${userEmail}/${plan}`)}
-          >
-            Checkout
-          </Button>
-        </Form>
-      </div>
     </div>
   );
 }
--- src/components/PlanSelection.js
+++ src/components/PlanSelection.js
@@ -1,52 +1,31 @@
 import React from "react";
-
+import ExplainButtons from "./ExplainButtons";
+import PlanRadioButtons from "./PlanRadioButtons";
+import { Button } from "react-bootstrap";
 export default function PlanSelection({ plan, setPlan }) {
   return (
-    <div>
-      <div className="flex flex-row justify-center items-center  ">
-        <div className="flex radio  w-auto">
-          <label className="btn btn-secondary active option">
-            <input
-              type="radio"
-              name="options"
-              value="basic"
-              onClick={() => {
-                setPlan("basic");
-              }}
-            />{" "}
-            Basic plan
-            <p className="box-dimension">Up to 5 boxes for free storage</p>
-          </label>
-          <label className="btn btn-secondary option">
-            <input
-              type="radio"
-              name="options"
-              value="premium"
-              onClick={() => {
-                setPlan("premium");
-              }}
-            />{" "}
-            Premium
-            <p className="box-dimension">Up to 10 boxes for free storage</p>
-          </label>
-        </div>
+    <div className="">
+      <div className="flex flex-row justify-center items-center ">
         {plan === "premium" ? (
-          <div className="flex flex-col ">
-            <div className="items flex  ">
-              <div className="col  ">
+          <div className="flex flex-col">
+            <div className="items flex ">
+              <div className="col mx-5  ">
                 <img
-                  className="h-60 w-60  animate-wiggle"
+                  className="h-60 w-80  animate-wiggle"
                   src={require("../pictures/boxes.png")}
                   alt="Picture for 10 boxes"
                 />
                 <img
-                  className=" h-60 w-60  animate-wiggle"
+                  className=" h-60 w-80  animate-wiggle"
                   src={require("../pictures/boxes.png")}
                   alt="Picture for 10 boxes"
                 />
               </div>
-              <div className="ex border-8 bg-gray-200 rounded-3xl shadow-lg ">
-                <h4 className="  text-center bg-gray-200 w-auto text-5xl">
+              <div className="ex border-8 bg-gray-200 rounded-3xl shadow-lg">
+                <div className="flex justify-center items-center">
+                  <PlanRadioButtons setPlan={setPlan} />
+                </div>
+                <h4 className="mt-3  text-center bg-gray-200 w-auto text-5xl">
                   Premium Plan JPY5,999
                 </h4>
                 <p className=" bg-gray-200 text-2xl break-words  ">
@@ -57,19 +36,23 @@ export default function PlanSelection({ plan, setPlan }) {
                 <div className="flex justify-center items-center bg-gray-300">
                   <img src={require("../pictures/PREMIUM.png")} alt="" />
                 </div>
+                <ExplainButtons />
               </div>
             </div>
           </div>
         ) : plan === "basic" ? (
           <div className="flex flex-col  ">
-            <div className="items flex ">
+            <div className="items flex  ">
               <img
-                className=" h-80 w-80  animate-wiggle"
+                className=" h-80 w-80  animate-wiggle mx-5"
                 src={require("../pictures/boxes.png")}
                 alt="Picture for 5 boxes"
               />
               <div className="ex border-8 bg-gray-200 rounded-3xl shadow-lg ">
-                <h4 className="   text-center bg-gray-200 w-auto text-5xl">
+                <div className="flex justify-center items-center">
+                  <PlanRadioButtons setPlan={setPlan} />
+                </div>
+                <h4 className="mt-3 text-center bg-gray-200 w-auto text-5xl">
                   Basic Plan JPY3,999
                 </h4>
                 <p className=" bg-gray-200 text-2xl break-words ">
@@ -79,34 +62,26 @@ export default function PlanSelection({ plan, setPlan }) {
                 <div className="flex justify-center items-center bg-gray-300">
                   <img src={require("../pictures/basic.png")} alt="" />
                 </div>
+                <ExplainButtons />
               </div>
             </div>
           </div>
         ) : (
-          // <div className="flex flex-col ">
-          //   <div className="items flex  ">
-          //     <div className="col  ">
-          //       <img
-          //         className="h-80 w-80  animate-wiggle"
-          //         src={require("../pictures/boxes.png")}
-          //         alt="Picture for 5 boxes"
-          //       />
-          //     </div>
-          //     <div className="ex border-8 bg-gray-200 rounded-3xl ">
-          //       <h4 className="  text-center bg-gray-200 w-auto text-5xl">
-          //         Basic Plan JPY3,999{" "}
-          //       </h4>
-          //       <p className=" bg-gray-200 text-2xl break-words  ">
-          //         This is the basic plan that allows you to store up to 5 boxes
-          //         without any extra monthly cost. Ideal for a single person.
-          //       </p>
-          //       <div className="flex justify-center items-center bg-gray-300">
-          //         <img src={require("../pictures/basic.png")} alt="" />
-          //       </div>
-          //     </div>
-          //   </div>
-          // </div>
-          ""
+          <div className="transition-all duration-300">
+            <div className="flex flex-col items-center justify-center bg-gray-200 rounded-3xl px-3 py-3 ">
+              <p className="mt-3 text-center bg-gray-200 w-auto text-5xl">
+                Let's start awaReHouse
+              </p>
+              <img
+                className="h-80 w-80 "
+                src={require("../pictures/LOGO.png")}
+                alt=""
+              />
+              <Button className="mx-3 my-3" onClick={() => setPlan("basic")}>
+                Start
+              </Button>
+            </div>
+          </div>
         )}
       </div>
     </div>
