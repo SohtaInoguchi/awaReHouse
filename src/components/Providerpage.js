0fe590d187fb91f474bf9c2a5827ba9f69a1ddd8
Etienne Boisnier <etienneboisnier@gmail.com>
2022-03-23 13:10:29 +0900

    Feat margin added below stored boxes on provider page

--- src/components/Providerpage.js
+++ src/components/Providerpage.js
@@ -401,9 +401,9 @@ calculateNextPayment()
         ) : (
           <div className="moreStorage">
             <form>
-              <h5 className="topFormtop">Please give us some details.</h5>
+              <h5 className="topFormtop">Please give us some details</h5>
               <label>
-                <h6 className="topForm">Where is this new storage capacity located?</h6>
+                <h6 className="topForm">Where is this new storage located?</h6>
                 <input
                   type="text"
                   name="location"
@@ -411,7 +411,7 @@ calculateNextPayment()
                   value={location}
                   onChange={createLocation}
                 />
-                <h6  className="topForm">What is the storage capacity offered (in m3)?</h6>
+                <h6  className="topForm">How much additional storage capacity is available? (in m3)</h6>
                 <input
                   type="text"
                   name="capacity"
@@ -419,7 +419,7 @@ calculateNextPayment()
                   value={capacity}
                   onChange={createCapacity}
                 />
-                <h6  className="topForm">On which floor is this new storage capacity located?</h6>
+                <h6  className="topForm">On which floor is this new storage located?</h6>
                 <input
                   type="text"
                   name="floor"
@@ -428,7 +428,7 @@ calculateNextPayment()
                   onChange={createFloorAddition}
                 />
 
-                <h6  className="topForm">Please indicate from when this additional capacity will be available</h6>
+                <h6  className="topForm">Please indicate from when this storage will become available</h6>
                 <DatePicker
                   selected={startDate}
                   onSelect={(date) => handleDateSelect(date)}
